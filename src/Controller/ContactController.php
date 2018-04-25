3eef478ce17ef6a86a69ce28143faa4aed32d392
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2018-04-26 00:47:41 +0300

    Sukuriame formą kontroleryje
    Ir padarome, kad išvestų formą į ekraną
    
    Dokumentacija:
    https://symfony.com/doc/current/forms.html

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ContactController extends Controller
+class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
     public function index()
     {
+        $form = $this->createForm(ContactType::class);
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'our_form' => $form->createView(),
         ]);
     }
 }
