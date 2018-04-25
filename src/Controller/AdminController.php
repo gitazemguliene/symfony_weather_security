2f0f0a31ef9bc25b3490d91023546fef84def4df
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2018-04-25 23:52:11 +0300

    Naudotojo objekto pasiekimas iš Kontrolerio
    Patogu, jei norima nukreipti į Prisijungimą arba neužtenka standartinių Security konfiguracijos galimybių.
    Naudotojo objektas įdedamas į kontrolerio metodą (analogiškai kaip Service'ams).
    
    Jei naudotojas(-a) neprisijungęs, reikia "UserInterface $user = null",
    kitaip Symfony nežinos ką daryti su null (neprisijungęs), kurio negalima paversti į UserInterface objektą.
    
    Pastaba:
      AccessDeniedException yra standartinis atsakymas, kai neprisiregistruojama, bet tada grąžinas HTTP 500 statusas.
      Produkcinėje aplinkoje reikėtų reaguoti į kernel.exception arba užsidėti templates/bundles/TwigBundle/Exception/error.html.twig žmogiškai klaidos žinutei
    
    Dokumentacija:
      https://symfony.com/doc/current/controller.html#redirecting
      https://symfony.com/blog/new-in-symfony-3-2-user-value-resolver-for-controllers
      https://symfony.com/doc/current/security.html#securing-controllers-and-other-code

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class AdminController extends Controller
 {
@@ -12,8 +15,27 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/review", name="review")
+     * @param UserInterface|null $user
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\Finder\Exception\AccessDeniedException
+     */
+    public function review(UserInterface $user = null): Response
+    {
+        if (!$user) {
+            throw new AccessDeniedException('Your user is not authorised');
+        }
+        if (\in_array('ROLE_MANO_ROLE', $user->getRoles(), true)) {
+            return $this->render('admin/review.html.twig');
+        }
+        if (\in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return $this->render('admin/plan.html.twig');
+        }
+        // Design system to not reach this line: produce HTTP 500 response
+        throw new AccessDeniedException('User not authorised by known roles');
     }
 }
